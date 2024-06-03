4c70b80a524c53aa131d5490658bc34379027d44
vivek-metricdust <vivek.v@metricdust.com>
2024-06-03 13:08:31 +0530

    added reading file from the git hub repos

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// async function fetchReadme() {
-async function fetchReadme(owner, repo) {
-    // const owner = "STORDIS";
-    // const repo = "monsoon";
+// function to fetch the readme.md from the git hub repository
 
+async function fetchReadme(owner, repo) {
     const readmeUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/README.md`;
 
     // Fetch the README file
@@ -17,17 +15,28 @@ async function fetchReadme(owner, repo) {
             images.forEach((img) => {
                 // If the image source is relative, convert it to an absolute URL
 
-                let temp = img.src.replace("http://127.0.0.1:5500", "");
+                let len = img.src.split("/").length;
+                let ary = img.src.split("/");
 
-                img.src = `https://raw.githubusercontent.com/STORDIS/monsoon/main/${temp}`;
+                img.src = `https://raw.githubusercontent.com/STORDIS/monsoon/main/${
+                    ary[len - 2]
+                }/${ary[len - 1]}`;
+            });
+
+            // add bootstrap class to table
+            const tables = document.querySelectorAll("#readme-content table");
+            tables.forEach((table) => {
+                table.classList.add("table");
+                table.classList.add("table-bordered");
             });
         })
         .catch((error) => {});
 }
 
-// fetchReadme();
-fetchReadme("STORDIS", "monsoon");
 
+fetchReadme("STORDIS", "orca_nw_lib");
+
+// function to make the side bar sticky when the page is scrolled down
 document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("sidebar");
     const sidebarTop = sidebar.offsetTop;
@@ -41,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// function to set the link active state
 function setActive(link) {
     const links = document.querySelectorAll(".sub-link");
     links.forEach((l) => l.classList.remove("active"));
