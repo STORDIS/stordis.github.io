3b75de513b074c1ee82ce584a3515ce87a2350fc
kamalkrbh <kamal.kr.bh@gmail.com>
2024-07-03 16:23:55 +0200

    Shifted html content to html files.

--- script.js
+++ script.js
@@ -1,126 +1,107 @@
 // function to fetch the readme.md from the git hub repository
 
 async function fetchReadme(owner, repo) {
-    const readmeUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/README.md`;
+  const readmeUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/README.md`;
 
-    // Fetch the README file
-    fetch(readmeUrl)
-        .then((response) => response.text())
-        .then((markdown) => {
-            const html = marked.parse(markdown);
+  // Fetch the README file
+  fetch(readmeUrl)
+    .then((response) => response.text())
+    .then((markdown) => {
+      const html = marked.parse(markdown);
 
-            document.getElementById("readme-content").innerHTML = html;
+      document.getElementById("readme-content").innerHTML = html;
 
-            const images = document.querySelectorAll("#readme-content img");
-            images.forEach((img) => {
-                // If the image source is relative, convert it to an absolute URL
+      const images = document.querySelectorAll("#readme-content img");
+      images.forEach((img) => {
+        // If the image source is relative, convert it to an absolute URL
 
-                console.log(img.src);
+        console.log(img.src);
 
-                let len = img.src.split("/").length;
-                let ary = img.src.split("/");
+        let len = img.src.split("/").length;
+        let ary = img.src.split("/");
 
-                if (
-                    ary[len - 1].includes(".jpg") ||
-                    ary[len - 1].includes(".png")
-                ) {
-                    img.src = `https://raw.githubusercontent.com/${owner}/${repo}/main/${
-                        ary[len - 2]
-                    }/${ary[len - 1]}`;
-                }else{
-                    img.style.width = 'auto';
-                }
-            });
+        if (ary[len - 1].includes(".jpg") || ary[len - 1].includes(".png")) {
+          img.src = `https://raw.githubusercontent.com/${owner}/${repo}/main/${
+            ary[len - 2]
+          }/${ary[len - 1]}`;
+        } else {
+          img.style.width = "auto";
+        }
+      });
 
-            // add bootstrap class to table
-            const tables = document.querySelectorAll("#readme-content table");
-            tables.forEach((table) => {
-                table.classList.add("table");
-                table.classList.add("table-bordered");
-            });
-        })
-        .catch((error) => {});
+      // add bootstrap class to table
+      const tables = document.querySelectorAll("#readme-content table");
+      tables.forEach((table) => {
+        table.classList.add("table");
+        table.classList.add("table-bordered");
+      });
+    })
+    .catch((error) => {});
 }
 
 //fetchReadme("STORDIS", "orca_nw_lib");
 
 // function to make the side bar sticky when the page is scrolled down
 document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.getElementById("sidebar");
-    const sidebarTop = sidebar.offsetTop;
+  const sidebar = document.getElementById("sidebar");
+  const sidebarTop = sidebar.offsetTop;
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY >= sidebarTop) {
-            sidebar.classList.add("fixed");
-        } else {
-            sidebar.classList.remove("fixed");
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= sidebarTop) {
+      sidebar.classList.add("fixed");
+    } else {
+      sidebar.classList.remove("fixed");
+    }
+  });
 });
 
 // function to set the link active state
 function setActive(id) {
-    const links = document.querySelectorAll(".sub-link");
-    links.forEach((l) => l.classList.remove("active"));
-    const link = document.getElementById(id);
-    if (link) {
-        link.classList.add("active");
-    }
+  const links = document.querySelectorAll(".sub-link");
+  links.forEach((l) => l.classList.remove("active"));
+  const link = document.getElementById(id);
+  if (link) {
+    link.classList.add("active");
+  }
+}
+function get_orca_content() {
+  return fetch("./orca_content.html")
+    .then((response) => response.text())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error loading HTML file:", error);
+    });
 }
 
-var orca_content = `<h1>ORCA - An OpenSource Orchestration Solution for SONiC</h1>
-<ul>
-<li> ORCA has following 3 Major components-
-<ul>
-<li class="link" id="orca_nw_lib" onclick="setActive('orca_nw_lib'),fetchReadme('STORDIS', 'orca_nw_lib')" >ORCA Network Library</li>
-<li class="link" id="orca_ui" onclick="setActive('orca_ui'), fetchReadme('STORDIS', 'orca_ui')" >ORCA Backend</li>
-<li class="link" id="orca_backend" onclick="setActive('orca_backend'), fetchReadme('STORDIS', 'orca_backend')" >ORCA UI</li>
-</ul>
-</li>
-<li>Network Topology maintained Neo4j graph database.</li>
-<li>Realtime updates in DB using gNMI subscriptions.</li>
-<li>ORCA can discover and configure -
-<ul>
-<li>Interfaces</li>
-<li>LLDP Neighbor Links</li>
-<li>Port Groups</li>
-<li>MCLAG with peer links</li>
-<li>BGP and Neighbor links</li>
-<li>VLAN</li>
-</ul>
-</li>
+function get_orca_video_content() {
+  return fetch("./orca_video_content.html")
+    .then((response) => response.text())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error loading HTML file:", error);
+    });
+}
 
-
-<li>Live Demo -
-<ul>
-<li><a href="http://orca.stordis.com:8880/">ORCA</a></li>
-<li><a href="http://orcadb.stordis.com:8881/">ORCA Topology Database</a></li>
-</ul>
-</li>
-
-
-
-</ul>
-<h3> ORCA Architecture </h3>
-<img class="diagram" src=orca_design.jpg alt="Orca Design"></img>
-<h2>ORCASK - AI Enabled chatbot for SONiC configuration</h2>
-<img class="diagram" src=orcask.jpg alt="Orca Design"></img>`;
-
-var orca_video_content = `
-<h2>ORCA Video Tutorials</h2>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/t38xiw1rfUs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/5aMdCqaim7A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-`;
-
-var monsoon_video_content = `
-<h2>ORCA Video Tutorials</h2>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/T1hULsw59n0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/VG8nhtzPtj4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-`;
+function get_monsoon_video_content() {
+    return fetch("./monsoon_video_content.html")
+      .then((response) => response.text())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error loading HTML file:", error);
+      });
+  }
 
 function loadContent(htmlContent) {
-    const contentContainer = document.getElementById("readme-content");
-    contentContainer.innerHTML = htmlContent;
+  const contentContainer = document.getElementById("readme-content");
+  contentContainer.innerHTML = htmlContent;
 }
 
-loadContent(orca_content);
+get_orca_content().then((content) => {
+  loadContent(content);
+});
